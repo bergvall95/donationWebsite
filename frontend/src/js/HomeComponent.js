81d0338d79704e2c06d663d4285bf4d737ab5072
cdfritz7 <cdfritz7@gmail.com>
2021-06-26 19:46:16 -0500

    added simple backend

--- frontend/src/js/HomeComponent.js
+++ frontend/src/js/HomeComponent.js
@@ -4,6 +4,24 @@ import "../css/HomeComponent.css";
 
 class HomeComponent extends Component {
 
+  constructor(props){
+    super(props)
+
+    this.state = {"testApiMessage": null}
+  }
+
+  componentDidMount(){
+    fetch("http://localhost:3001/api").then(
+      (data)=>{
+        return data.json()
+      }
+    ).then(
+      (data)=>{
+        this.setState({"testApiMessage": data.message})
+      }
+    )
+  }
+
   render() {
     return (
       <div>
@@ -11,6 +29,7 @@ class HomeComponent extends Component {
         <div className="home-container">
           <div className="heading">Welcome to Donation Station</div>
           <div className="text-medium">Where would you like to donate?</div>
+          <div className="text-medium">{this.state.testApiMessage}</div>
           <input className="search-bar" placeholder="Enter your zip code here"></input>
         </div>
 
